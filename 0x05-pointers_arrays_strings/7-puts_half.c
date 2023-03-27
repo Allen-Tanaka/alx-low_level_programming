6a37f58581574cb7a96ccd0455dfd0f93c63da7c
Allen <tkmzor@gmail.com>
2023-03-27 14:29:45 -0700

    half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,27 @@
 #include "main.h"
 
-
+/**
+ * puts_half - function printing half a string
+ * @str: string par
+ *
+ * Return: void
+ */
 
 void puts_half(char *str)
 {
-	
+	int i, s, longi = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		longi++;
+
+	s = longi / 2;
+
+	if (longi % 2 == 1)
+		s = (longi + 1) / 2;
+
+	for (i = s; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
