b8f591308af22cc3999219b8e3e21de984b038fa
Allen <tkmzor@gmail.com>
2023-03-30 13:27:29 -0700

    Capitalise

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalising words all lowercase
+ *
+ * @a: string to be capitalized
+ *
+ * Return: a
+ */
+
+char *cap_string(char *a)
+{
+	int i = 0;
+	int s = a[i - 1];
+
+	while (!((a[i] >= 'a') && (a[i] <= 'z')))
+		i++;
+
+	while (s == ' ' || s == '\t' || s == '\n' || s == ',' || s == ';' ||
+			s == '.' || s == '!' || s == '?' || s == '"' ||
+			s == '(' || s == ')' || s == '{' || s == '}' || i == 0)
+	{
+		a[i] = a[i] - 32;
+		i++;
+	}
+	return (a);
+
+}
