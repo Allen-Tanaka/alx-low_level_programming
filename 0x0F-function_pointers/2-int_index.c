7b20849c68bbf8d8ccd2d907faf16d2d92e62a51
Allen <tkmzor@gmail.com>
2023-04-20 02:39:00 -0700

    indexing

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,14 +15,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = 0;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
-		return(-1);
+		return (-1);
 	while (i < size)
 	{
 		if (cmp(array[i]))
 		{
-			return(i);
+			return (i);
 		}
 		i++;
 	}
-	return(-1);
+	return (-1);
 }
