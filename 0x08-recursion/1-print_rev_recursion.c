caab83d754fd55ab2e3fb7fdd9c122d9356926e8
Allen <tkmzor@gmail.com>
2023-04-20 11:51:11 -0700

    puts() functyion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - puts() function
+ *
+ * @s: iput
+ *
+ * Return: 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar("\n");
+}
