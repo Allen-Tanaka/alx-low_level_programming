8f72b039f5e21db36c18ecffc5e353452fedb5d9
Allen <tkmzor@gmail.com>
2023-04-27 11:33:37 -0700

    memory duplicate

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 
 /**
  * _strdup - duplicate to new memory space location
@@ -13,10 +13,10 @@
 char *_strdup(char *str)
 {
 	char *aaa;
-	int i = 0, r = 0;
+	int i = 0, r;
 
 	if (str == NULL)
-		return (NULL)
+		return (NULL);
 	while (str[i] != '\0')
 		i++;
 
@@ -24,7 +24,7 @@ char *_strdup(char *str)
 
 	if (aaa == NULL)
 		return (NULL);
-	for (r; str[r]; r++)
+	for (r = 0; str[r]; r++)
 		aaa[r] = str[r];
 	return (aaa);
 }
