a8df4b5c311a85d14350142ec5465fecdde00191
Allen <tkmzor@gmail.com>
2023-05-03 08:10:10 -0700

    _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - fn that allocates memory for an array
+ *
+ * @nmemb: number of elements in array
+ * @size: size of element
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(size * nmemb);
+
+	if (p == NULL)
+		return (NULL);
+	_memset(p, 0, nmemb * size);
+
+	return (p);
+}
+
+/**
+ * *_memset - fn filling memory with a constant byte
+ *
+ * @s: memory area
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
+
