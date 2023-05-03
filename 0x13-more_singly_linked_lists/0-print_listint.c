359c046d4f8ab07e189eae5005b9d337a536dea0
Allen <tkmzor@gmail.com>
2023-05-02 22:32:01 -0700

    print n of nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - function printing elements of linked list
+ *
+ * @h: linked list to be printed
+ *
+ * Return: n of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
