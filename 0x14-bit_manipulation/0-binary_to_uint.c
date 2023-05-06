cbb339ee5477f91d583973661cbdf9587f68e371
Allen <tkmzor@gmail.com>
2023-05-06 16:15:37 -0700

    convert

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i = 0;
-	unsigned int d = 0;
+	unsigned int j = 0;
 
 	if (!b)
 		return (0);
-
 	while (b[i])
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		d = 2 * d + (b[i] - '0');
+		j = 2 * j + (b[i] - '0');
 		i++;
 	}
-	return (d);
+	return (j);
 }
 
