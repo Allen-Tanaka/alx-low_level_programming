4e9f25fe5a10e061222c952daa40d70dcc5753a3
Allen <tkmzor@gmail.com>
2023-08-31 14:17:04 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,7 +14,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	1UL << index;
+	mask = 1UL << index;
 	*n = *n | mask;
 	return (1);
 }
